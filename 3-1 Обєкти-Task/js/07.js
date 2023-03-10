ef2f898b2fece590f58f2a02e3f9355e0dcbcd9b
OlexandrYaremchuk <70215770+OlexandrYaremchuk@users.noreply.github.com>
2023-03-10 15:04:36 +0200

    Add 3-1 Object Task

---
+++ 3-1 Обєкти-Task/js/07.js
@@ -0,0 +1,15 @@
+//*? Задача 7
+//*? Створіть функцію multiplyNumeric(obj), яка множить всі числові властивості обʼєкта на 2
+
+const menu = {
+  width: 200,
+  height: 300,
+  tittle: 'My menu',
+};
+
+//після виклику функції має бути
+// menu = {
+//     width: 400,
+//     height: 600,
+//     title: "My menu"
+// }
